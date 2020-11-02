b281504ccf4a239fa1f2d2ccc2c207575a7e5476
edwinkr9 <71083780+edwinkr9@users.noreply.github.com>
2020-11-02 14:59:38 -0600

    Estructura de la api

---
+++ FerroApp.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FerroApp.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ FerroApp.Infraestructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FerroApp.Infraestructure
+{
+    public class Class1
+    {
+    }
+}
